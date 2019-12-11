8501e8102247ee2388259fa6ba6d0bd8e97a06e2
STEPHEN WELCH <stephenwelch1927@yahoo.com>
2019-12-11 13:34:07 +0000

    Commit for project

---
+++ MobileAppProject/MobileAppProject/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileAppProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Drinks()); 
+        }
+
+        private void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Food());
+        }
+
+        private void ImageButton_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Music());
+        }
+
+        private void ImageButton_Clicked_3(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Wheelchair());
+        }
+    }
+}
